129b0edceb32dd7b9739e0b7afc88f52de5dff3f
Guus van der Schaaf <g.vanderschaaf@student.tudelft.nl>
2019-08-11 19:05:11 +0200

    Added rotation

--- editor/interactable.js
+++ editor/interactable.js
@@ -1,10 +1,16 @@
 var itemlist = [];
 var counter = 0;
 var scale = 1.5;
+var dragMode = true;
+var danspaal = false;
 
 var objects = [
     { name: "Eettafel", width: 120, height: 80, color: "#0000FF", border: "#000000" },
-    { name: "PA Podium", width: 120 * 2, height: 80 * 5, color: "#A4010E", border: "#000000" }
+    { name: "PA Podium", width: 120 * 2, height: 80 * 5, color: "#A4010E", border: "#000000" },
+    { name: "AK Blok", width: 80, height: 40, color: "#fcc603", border: "#000000" },
+    { name: "Danspodium", width: 40 * 6, height: 80 * 1.5, color: "#09ad00", border: "#000000" },
+    { name: "Feestbar klein", width: 180, height: 50, color: "#654321", border: "#000000" },
+    { name: "Feestbar groot", width: 360, height: 50, color: "#654321", border: "#000000" }
 ];
 
 $(document).ready(function () {
@@ -14,12 +20,47 @@ $(document).ready(function () {
 
     objects.forEach(addButton);
 
+    $('img').on('dragstart', function (event) { event.preventDefault(); });
+
     $('#download').click(function () {
         html2canvas(document.querySelector("#capture")).then(canvas => {
             var link = document.getElementById("download");
             link.href = canvas.toDataURL();
             link.download = "plattegrond.png";
         });
+    });
+
+    $('#change-mode').click(function () {
+        if (dragMode == true) {
+            dragMode = false;
+            $('#change-mode').val("Drag Mode");
+        } else {
+            dragMode = true;
+            $('#change-mode').val("Rotate Mode");
+        }
+    });
+
+    $('#danspaal').click(function () {
+        if (danspaal == true) {
+            danspaal = false;
+            $('.danspaal').remove();
+
+            $('#danspaal').val("Voeg danspaal toe");
+        } else {
+            danspaal = true;
+
+            var item = $("<div />", {
+                id: "danspaal-div",
+                "class": "danspaal",
+                style: "width: " + scaleSize(160) + "px; height: " + scaleSize(80) + "px; background-color: #09ad00; border: 5px solid black;"
+            });
+            item.text("Danspaal");
+            $(".container").append(item);
+
+            document.getElementById("danspaal-div").style.transform = "translate(" + scaleSize(560) + "px, " + scaleSize(360) + "px)";
+
+            $('#danspaal').val("Verwijder danspaal");
+        }
     });
 });
 
@@ -62,7 +103,59 @@ var onAdd = function (index) {
         $('#' + listID).remove();
     });
 
+    var resetRotation = $("<input />", {
+        type: "image",
+        alt: "Reset rotatie",
+        src: "./img/resetRotate.png",
+        "class": "image-button"
+    });
+    resetRotation.click(function () {
+        var element = document.getElementById(itemID);
+        element.dataset.angle = 0;
+        var x = (parseFloat(element.getAttribute('data-x')) || 0);
+        var y = (parseFloat(element.getAttribute('data-y')) || 0);
+
+        element.style.webkitTransform = element.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(0rad' + ')';
+    });
+
+    var rotateLeft = $("<input />", {
+        type: "image",
+        alt: "Roteer links",
+        src: "./img/rotateLeft.png",
+        "class": "image-button"
+    });
+    rotateLeft.click(function () {
+        var element = document.getElementById(itemID);
+
+        var angle = parseFloat(element.dataset.angle || 0);
+        angle = element.dataset.angle = angle - (Math.PI / 2);
+        var x = (parseFloat(element.getAttribute('data-x')) || 0);
+        var y = (parseFloat(element.getAttribute('data-y')) || 0);
+
+        element.style.webkitTransform = element.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + angle + 'rad' + ')';
+    });
+
+    var rotateRight = $("<input />", {
+        type: "image",
+        alt: "Roteer rechts",
+        src: "./img/rotateRight.png",
+        "class": "image-button"
+    });
+    rotateRight.click(function () {
+        var element = document.getElementById(itemID);
+
+        var angle = parseFloat(element.dataset.angle || 0);
+        angle = element.dataset.angle = angle + (Math.PI / 2);
+        var x = (parseFloat(element.getAttribute('data-x')) || 0);
+        var y = (parseFloat(element.getAttribute('data-y')) || 0);
+
+        element.style.webkitTransform = element.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + angle + 'rad' + ')';
+    });
+
     listItem.append(removeButton);
+    listItem.append(resetRotation);
+    listItem.append(rotateLeft);
+    listItem.append(rotateRight);
     listItem.append(obj.name + " " + (counter + 1));
     $('#item-list').append(listItem);
 
@@ -85,8 +178,73 @@ interact('.draggable')
         autoScroll: true,
 
         // call this function on every dragmove event
-        onmove: dragMoveListener
+        onmove: moveListener,
+        onstart: function (event) {
+            if (dragMode == true) return;
+            const element = event.target;
+            const rect = element.getBoundingClientRect();
+
+            // store the center as the element has css `transform-origin: center center`
+            element.dataset.centerX = rect.left + rect.width / 2;
+            element.dataset.centerY = rect.top + rect.height / 2;
+            // get the angle of the element when the drag starts
+            element.dataset.angle = getDragAngle(event);
+        },
+        onend: function (event) {
+            if (dragMode == true) return;
+            const element = event.target;
+
+            // save the angle on dragend
+            element.dataset.angle = getDragAngle(event);
+        }
     })
+
+interact('.draggable')
+    .on('doubletap', function (event) {
+        var angle = parseFloat(event.target.dataset.angle || 0);
+        angle = event.target.dataset.angle = angle + (Math.PI / 2);
+
+        var x = (parseFloat(event.target.getAttribute('data-x')) || 0);
+        var y = (parseFloat(event.target.getAttribute('data-y')) || 0);
+
+        event.target.style.webkitTransform = event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + angle + 'rad' + ')';
+
+        event.preventDefault()
+    })
+
+function getDragAngle(event) {
+    var element = event.target;
+    var startAngle = parseFloat(element.dataset.angle) || 0;
+    var center = {
+        x: parseFloat(element.dataset.centerX) || 0,
+        y: parseFloat(element.dataset.centerY) || 0,
+    };
+    var angle = Math.atan2(center.y - event.clientY,
+        center.x - event.clientX);
+
+    return angle - startAngle;
+}
+
+function moveListener(event) {
+    if (dragMode == true) {
+        dragMoveListener(event);
+    } else {
+        rotateMoveListener(event)
+    }
+}
+
+function rotateMoveListener(event) {
+    var element = event.target;
+    var center = {
+        x: parseFloat(element.dataset.centerX) || 0,
+        y: parseFloat(element.dataset.centerY) || 0,
+    };
+    var angle = getDragAngle(event);
+    var x = (parseFloat(element.getAttribute('data-x')) || 0);
+    var y = (parseFloat(element.getAttribute('data-y')) || 0);
+
+    element.style.webkitTransform = element.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + angle + 'rad' + ')';
+}
 
 function dragMoveListener(event) {
     var target = event.target
@@ -94,10 +252,12 @@ function dragMoveListener(event) {
     var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
     var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
+    var angle = target.dataset.angle || 0;
+
     // translate the element
     target.style.webkitTransform =
         target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)'
+        'translate(' + x + 'px, ' + y + 'px) rotate(' + angle + 'rad)'
 
     // update the posiion attributes
     target.setAttribute('data-x', x)
@@ -114,26 +274,26 @@ var exportHTML = function () {
 }
 
 function download(canvas, filename) {
-  /// create an "off-screen" anchor tag
-  var lnk = document.createElement('a'), e;
+    /// create an "off-screen" anchor tag
+    var lnk = document.createElement('a'), e;
 
-  /// the key here is to set the download attribute of the a tag
-  lnk.download = filename;
+    /// the key here is to set the download attribute of the a tag
+    lnk.download = filename;
 
-  /// convert canvas content to data-uri for link. When download
-  /// attribute is set the content pointed to by link will be
-  /// pushed as "download" in HTML5 capable browsers
-  lnk.href = canvas.toDataURL("image/png;base64");
+    /// convert canvas content to data-uri for link. When download
+    /// attribute is set the content pointed to by link will be
+    /// pushed as "download" in HTML5 capable browsers
+    lnk.href = canvas.toDataURL("image/png;base64");
 
-  /// create a "fake" click-event to trigger the download
-  if (document.createEvent) {
-    e = document.createEvent("MouseEvents");
-    e.initMouseEvent("click", true, true, window,
-                     0, 0, 0, 0, 0, false, false, false,
-                     false, 0, null);
+    /// create a "fake" click-event to trigger the download
+    if (document.createEvent) {
+        e = document.createEvent("MouseEvents");
+        e.initMouseEvent("click", true, true, window,
+            0, 0, 0, 0, 0, false, false, false,
+            false, 0, null);
 
-    lnk.dispatchEvent(e);
-  } else if (lnk.fireEvent) {
-    lnk.fireEvent("onclick");
-  }
+        lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+        lnk.fireEvent("onclick");
+    }
 }
