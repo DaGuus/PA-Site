8c39bb77ac2475170c59f3ed79ff3cd805e7aac0
Guus van der Schaaf <g.vanderschaaf@student.tudelft.nl>
2019-08-11 14:16:17 +0200

    Added sample objects

--- editor/interactable.js
+++ editor/interactable.js
@@ -1,18 +1,62 @@
 var itemlist = [];
 var counter = 0;
+var scale = 1.5;
 
-var onAdd = function () {
+var objects = [
+    { name: "Eettafel", width: 120, height: 80, color: "#0000FF", border: "#000000" },
+    { name: "PA Podium", width: 120 * 2, height: 80 * 5, color: "#A4010E", border: "#000000" }
+];
+
+$(document).ready(function () {
+    var addButton = function (obj, index, array) {
+        $('#button-list').append($('<input/>', { type: "button", value: obj.name, onclick: "onAdd(" + index + ");" }));
+    }
+
+    objects.forEach(addButton);
+});
+
+// 57 pixels is 100cm       213 * x / 375  - border size * 2 - padding * 2
+var scaleSize = function (x) {
+    return (((57 * x) / 100) * scale) - 10.0 - 20.0;
+}
+
+var onAdd = function (index) {
     var itemID = "item-" + counter;
-    $(".container").append("<div id=\"" + itemID + "\" class=\"draggable\"></div>");
+
+    var obj = objects[index];
+    var item = $("<div />", {
+        id: itemID,
+        "class": "draggable",
+        style: "width: " + scaleSize(obj.width) + "px; height: " + scaleSize(obj.height) + "px; background-color: " + obj.color + "; border: 5px solid " + obj.border + ";"
+    });
+    item.text(obj.name);
+
+
+    $(".container").append(item);
+    // $(".container").append("<div id=\"" + itemID + "\" class=\"draggable\"></div>");
     itemlist.push(itemID);
 
     var listID = itemID.replace("item-", "item-in-list-");
     var buttonID = itemID.replace("item-", "button-in-list-");
-    $('#item-list').append("<li id=\"" + listID + "\"> <input type=\"button\" value=\"" + itemID + "\" id=\"" + buttonID + "\" /> </li>");
-    $('#' + buttonID).click(function () {
+
+    var listItem = $("<li />", {
+        id: listID
+    });
+
+    var removeButton = $("<input />", {
+        type: "image",
+        alt: "Verwijder",
+        src: "./img/delete.png",
+        "class": "image-button"
+    });
+    removeButton.click(function () {
         $('#' + itemID).remove();
         $('#' + listID).remove();
     });
+
+    listItem.append(removeButton);
+    listItem.append(obj.name + " " + (counter + 1));
+    $('#item-list').append(listItem);
 
     counter++;
 }
