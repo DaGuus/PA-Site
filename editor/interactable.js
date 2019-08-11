cf8f10a96a4a3260e2db0be16ac69081af4c67af
Guus van der Schaaf <g.vanderschaaf@student.tudelft.nl>
2019-08-11 15:39:15 +0200

    Added png download

--- editor/interactable.js
+++ editor/interactable.js
@@ -13,6 +13,14 @@ $(document).ready(function () {
     }
 
     objects.forEach(addButton);
+
+    $('#download').click(function () {
+        html2canvas(document.querySelector("#capture")).then(canvas => {
+            var link = document.getElementById("download");
+            link.href = canvas.toDataURL();
+            link.download = "plattegrond.png";
+        });
+    });
 });
 
 // 57 pixels is 100cm       213 * x / 375  - border size * 2 - padding * 2
@@ -99,8 +107,33 @@ function dragMoveListener(event) {
 // this is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
 
-var exportHTML = function() {
+var exportHTML = function () {
     html2canvas(document.querySelector("#capture")).then(canvas => {
-        document.body.appendChild(canvas)
+        download(canvas, "plattegrond.png");
     });
+}
+
+function download(canvas, filename) {
+  /// create an "off-screen" anchor tag
+  var lnk = document.createElement('a'), e;
+
+  /// the key here is to set the download attribute of the a tag
+  lnk.download = filename;
+
+  /// convert canvas content to data-uri for link. When download
+  /// attribute is set the content pointed to by link will be
+  /// pushed as "download" in HTML5 capable browsers
+  lnk.href = canvas.toDataURL("image/png;base64");
+
+  /// create a "fake" click-event to trigger the download
+  if (document.createEvent) {
+    e = document.createEvent("MouseEvents");
+    e.initMouseEvent("click", true, true, window,
+                     0, 0, 0, 0, 0, false, false, false,
+                     false, 0, null);
+
+    lnk.dispatchEvent(e);
+  } else if (lnk.fireEvent) {
+    lnk.fireEvent("onclick");
+  }
 }
